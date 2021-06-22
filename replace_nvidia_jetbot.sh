17dd5cc5ca0f7d0c56cc873bc1fd9a432370597a
santaimpersonator <36016723+santaimpersonator@users.noreply.github.com>
2021-06-22 09:55:43 -0600

    Update replace_nvidia_jetbot.sh
    run script

--- replace_nvidia_jetbot.sh
+++ replace_nvidia_jetbot.sh
@@ -37,7 +37,8 @@
 # Remove Carriage Returns and Add Permissions
 echo -e "\e[100mRun Partition Expansion Script\e[0m"
 sed -i 's/\r$//' nvresizefs.sh
-chmod +x nvresizefs.sh
+# chmod +x nvresizefs.sh
+sudo bash nvresizefs.sh
 
 # Install pip3
 echo -e "\e[100mInstall pip3 htop nano\e[0m"
